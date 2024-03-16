8b59e227294d53f2083981f81e7f6e453f502884
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-03-16 09:58:49 +0700

    setup base server

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { PORT } from "./config-env.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { connectDB } from "./helpers/database.js";
+
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// db
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
