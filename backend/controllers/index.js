b10f2514197dc16ba77d39555284c8a89cf9e4e3
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-03-18 19:35:46 +0700

    setup base controller

---
+++ backend/controllers/index.js
@@ -0,0 +1,5 @@
+export * from "./auth.controller.js";
+export * from "./card.controller.js";
+export * from "./course.controller.js";
+export * from "./folder.controller.js";
+export * from "./user.controller.js";
--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { authMiddleware } from "../middlewares";
+import { cardController } from "../controllers";
 const cardRouter = express.Router();
 
-cardRouter.get("/", );
-cardRouter.get("/:id", );
-cardRouter.post("/", );
-cardRouter.put("/:id", );
-cardRouter.delete("/:id", );
+cardRouter.get("/", authMiddleware.verifyToken, cardController.getAll);
+cardRouter.get("/:id", authMiddleware.verifyToken, cardController.getOne);
+cardRouter.post("/", authMiddleware.verifyToken, cardController.create);
+cardRouter.put("/:id", authMiddleware.verifyToken, cardController.update);
+cardRouter.delete("/:id", authMiddleware.verifyToken, cardController.delete);
 
 export default cardRouter;
