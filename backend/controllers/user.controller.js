6ac7ff17cdfc4611efc44707c6ecec45bac62c5f
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-05-11 22:01:55 +0700

    fix user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,7 +16,7 @@ const getOne = async (req, res) => {
     const user = await User.findById(id);
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    return res.status(200).json(User);
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,7 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import userController from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
-userRouter.get("/", authMiddleware.verifyToken, userController.getOne);
+userRouter.get("/", authMiddleware.verifyToken, userController.getAll);
 userRouter.get("/:id", authMiddleware.verifyToken, userController.getOne);
 userRouter.post("/", authMiddleware.verifyToken, userController.create);
 userRouter.put("/:id", authMiddleware.verifyToken, userController.update);
