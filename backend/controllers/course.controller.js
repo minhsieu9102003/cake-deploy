d7e57d122e0152fcb8cb2852f27d8841c8d0a260
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-05-16 00:05:45 +0700

    fix get my course controller

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 
 const getAll = async (req, res) => {
   try {
-    const Courses = await Course.find();
-    return res.status(200).json(Courses);
+    const courses = await Course.find();
+    return res.status(200).json(courses);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
@@ -49,10 +49,10 @@ const getOne = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const Course = await Course.findById(id);
-    if (!Course) return res.status(404).json({ message: "Course not found" });
+    const course = await Course.findById(id);
+    if (!course) return res.status(404).json({ message: "Course not found" });
 
-    return res.status(200).json(Course);
+    return res.status(200).json(course);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
@@ -85,9 +85,9 @@ const deleteCourse = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const Course = await Course.findByIdAndDelete(id);
+    const course = await Course.findByIdAndDelete(id);
 
-    if (!Course) return res.status(404).json({ message: "Course not found" });
+    if (!course) return res.status(404).json({ message: "Course not found" });
 
     return res.status(200).json({ message: "Delete successfully!" });
   } catch (error) {
