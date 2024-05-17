c64fcf93c74c8e9790459ff9e04324f435730c3c
TDQM146 <137744432+TDQM146@users.noreply.github.com>
2024-05-17 17:47:34 +0700

    Delete Web-Group-24-minh-frontend directory

--- Web-Group-24-minh-frontend/backend/controllers/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./auth.controller.js";
-export * from "./card.controller.js";
-export * from "./course.controller.js";
-export * from "./folder.controller.js";
-export * from "./user.controller.js";
