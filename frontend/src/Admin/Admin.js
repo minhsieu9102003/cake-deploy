8f731e7a6ea75ef74fafc02d6fc7311c7b3b5b84
minhnt1409 <minh.nt200409@gmail.com>
2024-06-02 11:14:44 +0700

    hotfix layout logout

--- frontend/src/Admin/Admin.js
+++ frontend/src/Admin/Admin.js
@@ -241,7 +241,10 @@ const Admin = () => {
           <div className="avatar-dropdown">
             <img className="pfnavigation__avatar" src="/img/avatar2.png" alt="" />
             <div className="avatar-dropdown-content">
-              <button onClick={handleLogout}>Logout</button>
+              <button onClick={handleLogout}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" /></svg>
+                <span>Logout</span>
+              </button>
             </div>
           </div>
         </ul>
